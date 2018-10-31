9f3533df6a184ba8a18bff9d5974370514036057
Patrick Kellogg <patrickkellogg@gmail.com>
2018-10-31 09:35:08 -0400

    first commit

---
+++ kmd.js
@@ -0,0 +1,210 @@
+var orderlist = [];
+
+function zoomIn(numIn) {
+
+  let element = document.getElementById("overlay");
+  if (element.style.visibility == "visible") {
+    return;
+  }
+
+  element.innerHTML = "";
+  element.style.visibility = "visible";
+
+  for (let i = 0; i < prodlist.length; i++) {
+    if (prodlist[i].itemnum == numIn) {
+      let addtext = "<p>Item #" + prodlist[i].itemnum + "</p>" +
+      "<img src='" + prodlist[i].itemimg + "' width=300>" +
+      "<p>" + prodlist[i].itemdesc + "</p>";
+      element.insertAdjacentHTML('afterbegin', addtext);
+      break;
+    }
+  }
+
+  let mousex = event.clientX;
+  let mousey = event.clientY;
+  let elementheight = element.clientHeight;
+  let elementwidth = element.clientWidth;
+  let windowwidth = window.innerWidth;
+
+  if ((mousex < elementwidth) && (mousey < elementheight)){
+    //Doesn't fit on the left side
+    //See if fits on the right side
+    //i.e. smaller that windowwidth - elementwidth
+    console.log("Trying right");
+    if ((mousex > (windowwidth-elementwidth)) && (mousey < elementheight)) {
+      console.log("Right not ok");
+      element.style.visibility = "hidden";
+    } else {
+      console.log("Right ok");
+      //Doesn't fit
+      element.style.left = "" + (windowwidth-elementwidth) + "px";
+    }
+  } else {
+    //element.style.left = "0px";
+    console.log("Left ok");
+    element.style.left = "0px";
+  }
+}
+
+function zoomOut() {
+  var element = document.getElementById("overlay");
+  element.style.visibility = "hidden";
+}
+
+function addItem(newitemnum) {
+
+  zoomOut();
+
+  //Add the item to the orderlist
+  for (let i = 0; i < prodlist.length; i++) {
+    if (prodlist[i].itemnum == newitemnum) {
+      //Add it
+      orderlist.push(prodlist[i]);
+      //Remove it
+      prodlist.splice(i, 1);
+      break;
+    }
+  }
+
+  sortUL();
+  sortGrid();
+
+  showUL();
+  showGrid();
+
+}
+
+function showUL() {
+  //Clear the UL
+  document.getElementById("orderfull").innerHTML = "";
+
+  //If orderlist is empty, at least diplay "No items"
+  //And disable the clear and submit buttons
+  //Otherwise, enable the clear and submit buttons
+  if (orderlist.length > 0) {
+    document.getElementById("clearme").disabled = false;
+    document.getElementById("submitme").disabled = false;
+    document.getElementById("clearme").style.opacity = 1;
+    document.getElementById("clearme").style.cursor = "allowed";
+    document.getElementById("submitme").style.opacity = 1;
+    document.getElementById("submitme").style.cursor = "allowed";
+    document.getElementById("statusmessage").innerHTML = "Your Order";
+
+  } else {
+    document.getElementById("clearme").disabled = true;
+    document.getElementById("submitme").disabled = true;
+    document.getElementById("clearme").style.opacity = 0.25;
+    document.getElementById("clearme").style.cursor = "not-allowed";
+    document.getElementById("submitme").style.opacity = 0.25;
+    document.getElementById("submitme").style.cursor = "not-allowed";
+    document.getElementById("statusmessage").innerHTML = "Click on a page below to add to order";
+  }
+
+  let buildlist = "";
+
+  for (let i = 0; i < orderlist.length; i++) {
+    let newli = document.createElement("li");
+    let fullitem = document.createTextNode("Item #" + orderlist[i].itemnum + " " + orderlist[i].itemname);
+    newli.appendChild(fullitem);
+    //newli.onclick = deleteItem(orderlist[i].itemnum);
+    document.getElementById("orderfull").appendChild(newli);
+    if (buildlist == "") {
+      buildlist = orderlist[i].itemnum;
+    } else {
+      buildlist = buildlist+ " " + orderlist[i].itemnum;
+    }
+  }
+
+  document.getElementById("hiddenlist").value = buildlist;
+}
+
+function showGrid() {
+  //Clear the grid
+  document.getElementById("productlist").innerHTML = "";
+
+  for (let i = 0; i < prodlist.length; i++) {
+    let newdiv = document.createElement("div");
+    newdiv.id = prodlist[i].itemnum;
+    newdiv.onclick = function(){addItem(prodlist[i].itemnum)};
+    document.getElementById("productlist").appendChild(newdiv);
+    let fullitem = ("<p> Item #" +
+      prodlist[i].itemnum + "</p>" +
+      "<img src='" + prodlist[i].itemimg + "' width=100 " +
+      "onmousemove='zoomIn(" + prodlist[i].itemnum + ")' " +
+      "onmouseout='zoomOut()'>" +
+      "<p>" + prodlist[i].itemshort + "</p>");
+    newdiv.insertAdjacentHTML('afterbegin', fullitem);
+  }
+
+}
+
+function moveGridToUL(itemnumclicked) {
+
+    showUL();
+    showGrid();
+}
+
+function clickGrid() {
+
+  sortUL();
+  sortGrid();
+
+  showUL();
+  showGrid();
+}
+
+function clearList() {
+
+  orderlist = [];
+  prodlist = [];
+
+  for (let i = 0; i<savedlist.length; i++) {
+      prodlist[i] = {};
+      for (let prop in savedlist[i]) {
+          prodlist[i][prop] = savedlist[i][prop];
+      }
+  }
+
+  sortUL();
+  sortGrid();
+
+  showUL();
+  showGrid();
+}
+
+//Copied from stackoverflow.com
+//https://stackoverflow.com/questions/1129216/sort-array-of-objects-by-string-property-value
+function dynamicSort(property) {
+    let sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        let result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    };
+}
+
+function sortUL() {
+  orderlist = orderlist.sort(dynamicSort("itemnum"));
+}
+
+function sortGrid() {
+  prodlist = prodlist.sort(dynamicSort("itemnum"));
+}
+
+function startPage() {
+
+  showUL();
+  showGrid();
+
+  //Disable clean and submit buttons
+  document.getElementById("clearme").disabled = true;
+  document.getElementById("submitme").disabled = true;
+  document.getElementById("clearme").style.opacity = 0.25;
+  document.getElementById("clearme").style.cursor = "not-allowed";
+  document.getElementById("submitme").style.opacity = 0.25;
+  document.getElementById("submitme").style.cursor = "not-allowed";
+
+}
---
+++ kmdprodlist.js
@@ -0,0 +1,226 @@
+var prodlist = [];
+
+prodlist[0] = {'itemnum': 1,
+  'itemimg': 'images/1.png',
+  'itemname': 'Blank paper',
+  'itemshort': 'Blank',
+  'itemdesc': 'This letter-sized paper has no lines at all.'
+};
+
+prodlist[1] = {'itemnum': 2,
+  'itemimg': 'images/2.png',
+  'itemname': 'Wide-ruled paper (legal ruled paper)',
+  'itemshort': 'Wide-ruled',
+  'itemdesc': 'This letter-sized lined paper is wide ruled in portrait (vertical) orientation.'
+};
+
+prodlist[2] = {'itemnum': 3,
+  'itemimg': 'images/3.png',
+  'itemname': 'Narrow-ruled paper',
+  'itemshort': 'Narrow-ruled',
+  'itemdesc': 'This letter-sized lined paper is narrow ruled in portrait (vertical) orientation.'
+};
+
+prodlist[3] = {'itemnum': 4,
+  'itemimg': 'images/4.png',
+  'itemname': 'College-ruled paper (medium ruled)',
+  'itemshort': 'College-ruled',
+  'itemdesc': 'This letter-sized lined paper is college ruled in portrait (vertical) orientation.'
+};
+
+prodlist[4] = {'itemnum': 5,
+  'itemimg': 'images/5.png',
+  'itemname': 'Penmanship paper (Elementary paper, handwriting paper, DíNealian paper)',
+  'itemshort': 'Penmanship',
+  'itemdesc': 'Penmanship paper with ten sets of black, red, and blue lines in portrait orientation. Children learning to print or write cursive can use this dashed paper in school or at home to practice penmanship. It is letter-sized and has ten lines per page, in portrait (vertical) orientation.'
+};
+
+prodlist[5] = {'itemnum': 6,
+  'itemimg': 'images/6.png',
+  'itemname': 'Perforated Legal pad (Junior Size), Canary',
+  'itemshort': 'Legal pad',
+  'itemdesc': 'The notepad is perfect for taking notes and writing down appointments. Sheet size: 5" x 8".'
+};
+
+prodlist[6] = {'itemnum': 7,
+  'itemimg': 'images/7.png',
+  'itemname': 'Law Ruled paper',
+  'itemshort': 'Law Ruled',
+  'itemdesc': 'This letter-sized lined paper is law ruled and ideal for taking notes in law school or anywhere the wide left column setup will be of use.'
+};
+
+prodlist[7] = {'itemnum': 8,
+  'itemimg': 'images/8.png',
+  'itemname': 'Gregg-ruled Shorthand paper',
+  'itemshort': 'Gregg-ruled',
+  'itemdesc': 'Gregg-ruled shorthand paper with 11/32 inch line spacing and a red rule down the center. Perfect for note-taking.'
+};
+
+prodlist[8] = {'itemnum': 9,
+  'itemimg': 'images/9.png',
+  'itemname': 'Pitman-ruled Shorthand paper',
+  'itemshort': 'Pitman-ruled',
+  'itemdesc': 'Pitman-ruled shorthand paper with half-inch line spacing and a red rule down the center.'
+};
+
+prodlist[9] = {'itemnum': 10,
+  'itemimg': 'images/10.png',
+  'itemname': 'Cornell Note paper',
+  'itemshort': 'Cornell Note',
+  'itemdesc': 'This Cornell Notes paper is lined and additionally formatted with two large blank areas for a summary and a cue (on which to later note and review main ideas, ask questions, or draw diagrams.)'
+};
+
+prodlist[10] = {'itemnum': 11,
+  'itemimg': 'images/11.png',
+  'itemname': 'Genko Yoshi paper',
+  'itemshort': 'Genko Yoshi',
+  'itemdesc': 'This genkouyoushi squared paper is used to write compositions using a grid. It originates in Japan, where it is used to practice writing. One kanji, hiragana or katakana character fits in each square. One paper has 400 squares.'
+};
+
+prodlist[11] = {'itemnum': 12,
+  'itemimg': 'images/12.png',
+  'itemname': 'Tian Zi Ge paper',
+  'itemshort': 'Tian Zi Ge',
+  'itemdesc': 'This is divided into four sections. You can see this Chinese writing paper tempate used in primary school for the introduction of new characters with simple layouts.'
+};
+
+prodlist[12] = {'itemnum': 13,
+  'itemimg': 'images/13.png',
+  'itemname': 'Mi Zi Ge paper',
+  'itemshort': 'Mi Zi Ge',
+  'itemdesc': 'This is often used as Chinese calligraphy paper. The shape of the grid guidelines look like the Chinese character for rice.'
+};
+
+prodlist[13] = {'itemnum': 14,
+  'itemimg': 'images/14.png',
+  'itemname': 'French Ruled (Séyès Ruled)',
+  'itemshort': 'French Ruled',
+  'itemdesc': 'This French ruled paper, known also as Séyès Ruled paper, has lines 2 mm apart, with darker lines every 8 mm, and is on A4 sized paper.'
+};
+
+prodlist[14] = {'itemnum': 15,
+  'itemimg': 'images/15.png',
+  'itemname': 'Korean Manuscript Paper',
+  'itemshort': 'Korean Manuscript',
+  'itemdesc': 'This printable manuscript paper features diamond grids for learning to write Korean.'
+};
+
+prodlist[15] = {'itemnum': 16,
+  'itemimg': 'images/16.png',
+  'itemname': 'Grand Staff Music paper (manuscript paper, music paper)',
+  'itemshort': 'Grand Staff Music',
+  'itemdesc': 'This letter-sized music (manuscript) paper shows the grand staff and is in portrait (vertical) orientation.'
+};
+
+prodlist[16] = {'itemnum': 17,
+  'itemimg': 'images/17.png',
+  'itemname': 'Chord Chart for 6-string instrument',
+  'itemshort': 'Chord Chart',
+  'itemdesc': 'This letter-sized chord chart shows five frets and can be used with six-string instruments.'
+};
+
+prodlist[17] = {'itemnum': 18,
+  'itemimg': 'images/18.png',
+  'itemname': 'Graph paper (quadrille ruled paper, quad ruled paper, coordinate paper, grid paper, squared paper)',
+  'itemshort': 'Graph',
+  'itemdesc': 'This letter-sized graph paper has five aqua blue lines every inch plus heavy index lines every inch.'
+};
+
+prodlist[18] = {'itemnum': 19,
+  'itemimg': 'images/19.png',
+  'itemname': 'Semi-log paper with linear horizontal axis and logarithmic vertical axis (one decade)',
+  'itemshort': 'Semi-log',
+  'itemdesc': 'This letter-sized semi-log paper has a linear horizontal axis and logarithmic vertical axis (one decade). It is in portrait (vertical) orientation.'
+};
+
+prodlist[19] = {'itemnum': 20,
+  'itemimg': 'images/20.png',
+  'itemname': 'Log-log paper with logarithmic horizontal axis (one decade) and logarithmic vertical axis (one decade) with equal scales',
+  'itemshort': 'Log-log',
+  'itemdesc': 'This letter-sized log-log paper has a logarithmic horizontal axis (one decade) and logarithmic vertical access(one decade) and has equal scales. It is in portrait (vertical) orientation.'
+};
+
+prodlist[20] = {'itemnum': 21,
+  'itemimg': 'images/21.png',
+  'itemname': 'Dot paper (dot grid paper) with five dots per inch spacing on letter-sized paper',
+  'itemshort': 'Dot',
+  'itemdesc': 'This printable dot paper has five dots per inch and is in portrait orientation on letter-sized paper.'
+};
+
+prodlist[21] = {'itemnum': 22,
+  'itemimg': 'images/22.png',
+  'itemname': 'Polar-log paper',
+  'itemshort': 'Polar-log',
+  'itemdesc': 'This printable polar graph paper incorporates one decade of logarithmic concentric circles and spokes positioned 15 degrees apart. One Decade Fifteen Degrees.'
+};
+
+prodlist[22] = {'itemnum': 23,
+  'itemimg': 'images/23.png',
+  'itemname': 'Polar graph paper',
+  'itemshort': 'Polar graph',
+  'itemdesc': 'This letter-sized polar graph paper has 15-degree angles and quarter-inch radials plus heavy index lines on letter-sized paper.'
+};
+
+prodlist[23] = {'itemnum': 24,
+  'itemimg': 'images/24.png',
+  'itemname': 'Smith Chart',
+  'itemshort': 'Smith Chart',
+  'itemdesc': 'The Smith Chart is used by electrical and electronics engineers to aid in demonstrating and solving problems in radio frequency engineering.'
+};
+
+prodlist[24] = {'itemnum': 25,
+  'itemimg': 'images/25.png',
+  'itemname': 'Isometric Graph paper',
+  'itemshort': 'Isometric Graph',
+  'itemdesc': 'This letter-sized isometric graph paper has quarter-inch figures (triangles).'
+};
+
+prodlist[25] = {'itemnum': 26,
+  'itemimg': 'images/26.png',
+  'itemname': 'Hexagon Graph paper',
+  'itemshort': 'Hexagon Graph',
+  'itemdesc': 'This letter-sized hexagon graph paper is spaced with hexagons a quarter inch apart.'
+};
+
+prodlist[26] = {'itemnum': 27,
+  'itemimg': 'images/27.png',
+  'itemname': 'Slant Ruled paper',
+  'itemshort': 'Slant Ruled',
+  'itemdesc': 'This slant ruled paper features wide-ruled lines for writing rotated slightly counterclockwise. Wide Ruled Right Handed, Low Angle.'
+};
+
+prodlist[27] = {'itemnum': 28,
+  'itemimg': 'images/28.png',
+  'itemname': 'Impedance Graph paper (Reactance paper)',
+  'itemshort': 'Impedance Graph',
+  'itemdesc': 'Impedance Paper, also known as Reactance Frequency Graph Paper or just Reactance Paper, is useful for electronics engineers.'
+};
+
+prodlist[28] = {'itemnum': 29,
+  'itemimg': 'images/29.png',
+  'itemname': 'Triangular Coordinate paper',
+  'itemshort': 'Triangular Coordinate',
+  'itemdesc': 'This triangular coordinate graph paper involves a grid of triangles, and is similar to isometric graph paper in that it can be used to represent three-dimensional views. It can also be used with embroidery needlework projects.'
+};
+
+prodlist[29] = {'itemnum': 30,
+  'itemimg': 'images/30.png',
+  'itemname': 'Wulff Net paper',
+  'itemshort': 'Wulff Net',
+  'itemdesc': 'This printable graph paper is of a sphere projected onto a plane.'
+};
+prodlist[30] = {'itemnum': 31,
+  'itemimg': 'images/31.png',
+  'itemname': 'Columnar Paper with two columns (accounting paper, finance paper)',
+  'itemshort': 'Columnar Paper',
+  'itemdesc': 'This printable finance paper, like that found in a columnar pad, is letter-sized in landscape (horizontal) orientation and has two columns of green-shaded fields.'
+};
+
+var savedlist = [];
+
+for (var i = 0; i<prodlist.length; i++) {
+    savedlist[i] = {};
+    for (var prop in prodlist[i]) {
+        savedlist[i][prop] = prodlist[i][prop];
+    }
+}
